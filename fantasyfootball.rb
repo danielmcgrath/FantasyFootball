f5cc7107735511ff33774d47f0bb58e7ec432e25
Daniel McGrath <daniel@venodesigns.net>
2010-05-05 23:31:07 -0400

    Adding in half-baked getProjection and getInjuries methods.

--- fantasyfootball.rb
+++ fantasyfootball.rb
@@ -7,17 +7,15 @@ class FantasyFootball
         @api_key = "?apiKey=" + api_key
     end
     
-    def parseXML(action)
-        load = @url + action + @api_key
+    def parseXML(action, params)
+        load = @url + action + @api_key + params
         xml = open(load).read()
         return Hpricot(xml)
     end
 
     def getSchedule
         results = Array.new
-        # Using stored local copy of results while testing
-        # doc = parseXML("ffnScheduleXML.php")
-        doc = parseXML("/Users/daniel/Desktop/sample_nfl_data.xml")
+        doc = parseXML("ffnScheduleXML.php")
         
         (doc/"game").each do |game|
             curr = { 
@@ -29,6 +27,33 @@ class FantasyFootball
                 "gametime" => game.attributes['gametime']
             }
             results << curr
+        end
+        
+        return results
+    end
+    
+    # TODO: getProjection doesn't seem to actually return position
+    # projections. Bad API call or bad API?
+    def getProjection(week, position)
+        results = Array.new
+        doc = parseXML("ffnSitStartXML.php", "&week=" + week + "&position=" + position)
+        return doc # Haven't bothered parsing yet
+    end
+    
+    def getInjuries(week)
+        results = Array.new
+        doc = parseXML("ffnInjuriesXML.php", "&week=" + week)
+        if doc/"error" 
+            puts (doc/"error").inner_html
+        else
+            (doc/"injury").each do |injury|
+                curr = {
+                    # Since the season hasn't started and I can't get an
+                    # injury report, I'm not sure what the data format for
+                    # this is. Hopefully I can find a sample somewhere
+                }
+                results << curr
+            end
         end
         
         return results
